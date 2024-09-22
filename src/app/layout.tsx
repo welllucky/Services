2f0068367458f76f6fd4c55c83777e241ad7a119
Wellington Braga <wellingtonbat.wl@gmail.com>
2024-09-22 14:34:48 -0300

    build: :wrench: Atualiza os scripts de flow record e do sentry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,11 +35,14 @@ const RootLayout = ({
   <html lang="pt-br">
     <head>
       {(process.env.NODE_ENV === "development" ||
-        process.env.VERCEL_ENV === "preview") && (
+        process.env.VERCEL_ENV === "preview" ||
+        process.env.HOST_ENV !== "local") && (
         // eslint-disable-next-line @next/next/no-sync-scripts
         <script
           data-project-id="WfTqP43E0gdOt1orJkAjWq1d92P8Fo0Yn3iMHQtz"
-          data-is-production-environment="false"
+          data-is-production-environment={
+            process.env.NODE_ENV === "production" ? "true" : "false"
+          }
           src="https://snippet.meticulous.ai/v1/meticulous.js"
         />
       )}
