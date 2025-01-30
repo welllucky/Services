69ab79a79858e47353e26eb9ab4a6d3729896e99
Wellington Braga <wellington.braga@fcxlabs.com.br>
2025-01-29 23:10:09 -0300

    feat: :sparkles: Atualiza configuração do Playwright, ajusta branches do workflow e remove arquivos de relatório obsoletos

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,21 +4,21 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-
+  testDir: "./tests/e2e",
   // Folder for test artifacts such as screenshots, videos, traces, etc.
   outputDir: "./tests/playwright/output/",
 
   // path to the global setup files.
   // globalSetup: require.resolve('./global-setup'),
-
-  testDir: "./tests/e2e/",
   testMatch: "**/?(*.)+(e2e|integration).[tj]s?(x)",
 
   /* Run tests in files in parallel */
@@ -31,7 +31,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 1 : 4,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
@@ -48,7 +48,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL ?? "http://localhost:3000",
+    baseURL: "http://localhost:2424",
+    // baseURL: process.env.BASE_URL ?? "http://localhost:3000",
 
     // Record trace only when retrying a test for the first time.
     trace: "retain-on-failure",
@@ -155,11 +156,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "yarn dev",
-    url: process.env.BASE_URL ?? "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-    timeout: 5000,
-    // reuseExistingServer: true
-  },
+  // webServer: {
+  //   command: "yarn dev",
+  //   url: process.env.BASE_URL ?? "http://localhost:3000",
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 5000,
+  //   // reuseExistingServer: true
+  // },
 });
