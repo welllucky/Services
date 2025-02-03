880be910563814c1d3908d60d9cdf412865f7c78
Wellington Braga <wellingtonbat.wl@gmail.com>
2025-02-02 23:19:23 -0300

    chore: :arrow_up: Atualiza dependências do Sentry e Zustand para versões mais recentes

--- src/screens/Login/UI/components/Form/ForgotPassword/index.tsx
+++ src/screens/Login/UI/components/Form/ForgotPassword/index.tsx
@@ -2,6 +2,6 @@ import { ForgotPasswordContainer, ForgotPasswordLink } from "./styles";
 
 export const ForgotPassword = () => (
   <ForgotPasswordContainer>
-    <ForgotPasswordLink href="/recover">Esqueci a Senha</ForgotPasswordLink>
+    <ForgotPasswordLink href="/">Esqueci a Senha</ForgotPasswordLink>
   </ForgotPasswordContainer>
 );
