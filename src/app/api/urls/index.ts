dd7d6e839b9dd0c340061334c4c6c2455e9e92e1
Wellington Braga <wellingtonbat.wl@gmail.com>
2025-08-24 07:46:48 -0300

    fix: :bug: Muda o endpoint de autenticação da aplicação

--- src/app/api/urls/index.ts
+++ src/app/api/urls/index.ts
@@ -2,7 +2,7 @@ const baseUrl = process.env.APIS_BASE_URL ?? "";
 
 export const resolveUrl = `${baseUrl}/v1/core/resolve`;
 export const ticketUrl = `${baseUrl}/v1/core/tickets`;
-export const accountUrl = `${baseUrl}/v1/core/account`;
+export const accountUrl = `${baseUrl}/v1/account`;
 export const userUrl = `${baseUrl}/v1/core/users`;
 export const sectorUrl = `${baseUrl}/v1/core/sector`;
 export const roleUrl = `${baseUrl}/v1/core/role`;
